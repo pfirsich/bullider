b15b9d7c65d8a41a09381a5ced35ef882c181f1a
Joel Schumacher <joelschum@gmail.com>
2020-05-21 00:32:45 +0200

    Convert indentation to spaces

--- movingaverage.lua
+++ movingaverage.lua
@@ -1,24 +1,24 @@
 return function(count)
-	local values = {}
-	local nextIndex = 1
+    local values = {}
+    local nextIndex = 1
 
-	for i = 1, count do
-		values[i] = 0
-	end
+    for i = 1, count do
+        values[i] = 0
+    end
 
-	return function(val)
-		if val then
-			values[nextIndex] = val
-			nextIndex = nextIndex + 1
-			if nextIndex > count then
-				nextIndex = 1
-			end
-		else
-			local sum = 0
-			for i = 1, count do
-				sum = sum + values[i]
-			end
-			return sum / count
-		end
-	end
+    return function(val)
+        if val then
+            values[nextIndex] = val
+            nextIndex = nextIndex + 1
+            if nextIndex > count then
+                nextIndex = 1
+            end
+        else
+            local sum = 0
+            for i = 1, count do
+                sum = sum + values[i]
+            end
+            return sum / count
+        end
+    end
 end
